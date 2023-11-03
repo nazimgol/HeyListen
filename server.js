1fa658b282ee6df6645900b299e39a3d0ffa5be5
RichardHayes <rhshadowgreen@gmail.com>
2023-11-02 19:53:44 -0700

    init

---
+++ server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import { createServer } from 'node:http';
+import { Server } from 'socket.io';
+import path from 'path';
+import url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+io.on('connection', (socket) => {
+  socket.on('message', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`server running at http://localhost:${port}`);
+});
