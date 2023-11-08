66de9fb2a846ef2a50f1e8629ed7fc1626c7b379
Richard <rhshadowgreen@gmail.com>
2023-11-08 15:28:24 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@
 # 1 GITHUB_USER
 # 2 REPO_NAME
 
+# Command
+# ssh -i ~/.ssh/id_rsa root@host "bash -s" < deploy.sh GITHUB_USER REPO_NAME
+# TEST: ssh -i ~/.ssh/id_rch root@linode4.rch.app "bash -s" < deploy.sh strawstack heylisten
+
 git config --global user.name "Github Actions"
 git config --global user.email "actions@github.com"
 
